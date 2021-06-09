3f66fec66f873c80a59fba3abbd29b6608a5f325
Joe Pedroza <jpedroza50@yahoo.com>
2021-06-09 11:58:01 -0500

    Set covid in settings.py

--- covid/views.py
+++ covid/views.py
@@ -4,8 +4,7 @@ from django.http import HttpResponse
 from .models import Greeting
 
 # Create your views here.
-def index(request):
-    # return HttpResponse('Hello from Python!')
+def index(request):    
     return render(request, "index.html")
 
 
